aab68fe0a99e2158dec5dbc145a008a823227d60
NicholasSobchak <nlsobchak@gmail.com>
2026-08-06 00:46:13 -0400

    Fix types errors raised via npm run typecheck

--- ios/components/useClientOnlyValue.web.ts
+++ ios/components/useClientOnlyValue.web.ts
@@ -4,9 +4,6 @@ const emptySubscribe = () => () => {};
 
 // Returns the client value after hydration, or the server value during SSR.
 export function useClientOnlyValue<S, C>(server: S, client: C): S | C {
-  return useSyncExternalStore(
-    emptySubscribe,
-    () => client,
-    () => server,
-  );
+  const getServerSnapshot = () => server as S | C;
+  return useSyncExternalStore(emptySubscribe, () => client as S | C, getServerSnapshot);
 }
